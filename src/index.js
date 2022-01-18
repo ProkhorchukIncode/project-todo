e2df3503a141bb8d1d3be78c07e0505d05127552
qwerty:-) <danil.prokhorchuk@incode-it.com>
2022-01-18 15:55:31 +0200

    refactor index.js browser-router

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
 
 import App from './App';
 
@@ -10,7 +11,9 @@ import '@fontsource/roboto/700.css';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
