352e52c3d76a74d7901ae8191ff1fb6be06fb95f
qwerty:-) <danil.prokhorchuk@incode-it.com>
2022-01-18 15:41:05 +0200

    refactor app.js routes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
+import { Route, Routes, Navigate } from "react-router-dom";
+
+//autorisation
+const auth = false
 
 function App() {
   return (
     <>
+      {auth 
+        ? 
+        (<Routes>
+          <Route path='/registration' element ={<></>}/>
+          <Route path='/login' element ={<></>}/>
+          <Route path='*' element ={<Navigate replace to='/registration'/>}/>
+        </Routes>)
+        :
+        (<Routes>
+          <Route path='/home' element ={<></>}/>
+          <Route path='/todo/:id' element ={<></>}/>
+          <Route path='*' element ={<Navigate replace to='/home'/>}/>
+        </Routes>)
+      }
     </>
   );
 }
