9372e106a7da9265e3bd11b474ef9aa61c80e540
qwerty:-) <danil.prokhorchuk@incode-it.com>
2022-01-20 15:32:54 +0200

    feat(RegistrationPage,LoginPage): added functional inputs

--- src/Pages/PublicPages/RegistrationPage/index.jsx
+++ src/Pages/PublicPages/RegistrationPage/index.jsx
@@ -1,9 +1,30 @@
+import { useState } from "react";
+
 import Box from "@mui/system/Box";
 import Typography from "@mui/material/Typography";
 import TextField from "@mui/material/TextField";
 import Button from "@mui/material/Button";
 
 const RegistrationPage = () => {
+    const [username, setUsername] = useState('')
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+
+    const onChange = (event) => {
+        switch (event.target.id) {
+            case username:
+                setUsername(event.target.value)
+                break;
+            case email:
+                setEmail(event.target.value)
+                break;
+            case password:
+                setPassword(event.target.value)
+                break;
+            default:
+                break;
+        }
+    }
 
   return (
         <Box>
@@ -12,13 +33,15 @@ const RegistrationPage = () => {
             </Typography>
             <Box component="form">
                 <TextField 
-                    id="name"
+                    onChange = {onChange}
+                    id="username"
                     label="Your name"
-                    name="name"
+                    name="username"
                     autoComplete="name"
                     required
                 />
                 <TextField 
+                    onChange = {onChange}
                     id="email"
                     label="Email Address"
                     name="email"
@@ -26,6 +49,7 @@ const RegistrationPage = () => {
                     required
                 />
                 <TextField 
+                    onChange = {onChange}
                     id="password"
                     label="Password"
                     name="password"
