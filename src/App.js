4c7429f130adfee5d28f9589b573ea21ca346095
qwerty:-) <danil.prokhorchuk@incode-it.com>
2022-01-21 16:43:22 +0200

    feat: added refresh token

--- src/App.js
+++ src/App.js
@@ -1,35 +1,62 @@
 import { Route, Routes, Navigate } from "react-router-dom";
 import { useState, useEffect } from "react";
+// import { useDispatch, useSelector } from "react-redux";
 
 import NavigationBar from "./Components/NavigationBar";
 import RoutesComponent from "./Components/RoutesComponent";
 
 import ROUTES from "./Routes/routes";
 
+import axios from "axios";
+import axiosInstance from './HttpServices/axiosInstance'
+import { refreshToken } from "./Redux/user/userSlice";
+import createSetAuthInterceptor from "./HttpServices/axiosInterceptor";
+import axiosResponse from "./HttpServices/axiosResponse";
 
 import Container from "@mui/material/Container";
 
-//autorisation
-const auth = true
+
 
 function App() {
   const [routes, setRoutes] = useState(ROUTES.PUBLIC_ROUTES)
+  // const dispatch = useDispatch;
+  // const authorization = useSelector((state) => state.user.auth)
+  
+  const token = localStorage.getItem('token')
+  //
+  const a = () => axios.get(`http://localhost:5000/todos`)
+  a()
 
-  const isAuth = (auth) => {
-    if(auth){
-        setRoutes(ROUTES.PRIVATE_ROUTES)
-        return
+  axiosInstance.interceptors.request.use(createSetAuthInterceptor(token));
+  axios.interceptors.response.use(
+    (res) => {
+      return res;
+   },
+    (err) => {
+      let error = err
+      if(error.response.status===401){
+        refreshToken()
+      }
+      return {}
+   }
+    )
+//
+  const isAuth = (authorization) => {
+    if(authorization){
+      setRoutes(ROUTES.PRIVATE_ROUTES)
+      return
     }
     setRoutes(ROUTES.PUBLIC_ROUTES)
     return
   }
   
   useEffect(()=> {
-    isAuth(auth)
-  },[auth])
+    // dispatch(auth())
+    isAuth(token)
+  },[token])
 
   return (<>
-      <NavigationBar auth ={auth}/>
+      <NavigationBar auth ={token}/>
       <Container>
         <RoutesComponent>
           <Routes>
